8761954fdcbbb5fd51835dde130d2b803867c1b9
Denisoov <denisoov07@gmail..com>
2022-04-09 00:01:52 +0500

    добавил api запросы

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,150 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
+  <Page>
+    <ActionBar>
+      <Label text="dollarPrice" />
+    </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+    <TabView
+      height="100%"
+      textFieldBackgroundColor="#07bb58"
+      android-tabs-position="bottom"
+      tab-text-font-size="16"
+    >
+      <TabViewItem title="Актеры">
+        <StackLayout orientation="vertical" width="100%" height="100%">
+            <FlexboxLayout class="card" v-for="(actor, index) in actors" :key="index" > 
+                <Image 
+                    width="70" 
+                    height="70" 
+                    class="card__image" 
+                    :src="actor.img" 
+                    stretch="fill" 
+                />         
+                <Label
+                    width="150" height="70"
+                    :text="actor.name"
+                    class="card__title"
+                />
+            </FlexboxLayout>   
+        </StackLayout>
+      </TabViewItem>
+
+      <TabViewItem title="список вопросов">
+        <StackLayout orientation="vertical" width="100%" height="100%">
+            <Label
+                width="100%"
+                :text="joke"
+                class="card__title"
+                text-wrap="true"
+
+            />
+             <Button text="другая шутка" @tap="getJoke" />
+        
+        </StackLayout>
+      </TabViewItem>
+      <TabViewItem title="Курс рубля к $">
+        <StackLayout orientation="vertical" width="100%" height="100%">
+            <FlexboxLayout class="card">      
+                <Label
+                    :text="courses.Name"
+                    class="card__title"
+                    text-wrap="true"
+                />  
+                <Label
+                    :text="courses.Value"
+                    class="card__title"
+                    text-wrap="true"
+                />  
+            </FlexboxLayout>   
+          <Button text="Обновить курс" @tap="getCourses" />
+        </StackLayout>
+      </TabViewItem>
+    </TabView>
+  </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
+
+import { Http } from "@nativescript/core";
+
+export default {
+  data() {
+    return {
+      dollarPrice: { dollar: 2.81 },
+      actors: [
+          {
+            img: '',
+            name: ''
+          }
+      ],
+      joke: '',
+      courses: {
+          Value: '',
+          Name: ''
+      },
+    };
+  },
+  created() {
+    this.getActors();
+    this.getJoke();
+    this.getCourses();
+  },
+  methods: {
+    getActors() {
+      Http.getJSON("https://www.breakingbadapi.com/api/characters/?limit=7")
+        .then((response) => {
+            this.actors = response
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    },
+    getJoke() {
+      Http.getJSON("https://geek-jokes.sameerkumar.website/api?format=json")
+        .then((response) => {
+          this.joke = response.joke;
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    },
+    getCourses() {
+      Http.getJSON("https://www.cbr-xml-daily.ru/daily_json.js")
+        .then((response) => {
+          this.courses = response.Valute.USD;
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    },
+  },
+};
 </script>
 
-<style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
-
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
+<style lang="scss">
+Button {
+  font-size: 12;
+  font-weight: bold;
+  color: white;
+  background-color: #1a2c95;
+  padding: 10;
+  border-radius: 20px;
+  margin-top: 50px;
+}
+.card {
+    margin-bottom: 5px;
+    margin-top: 5px;
+    .card__image {
+        margin-left: 5px;
     }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+}
+.card__title {
+    padding-left: 40px;
+    padding-top: 75px;
+    font-weight: 600;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
